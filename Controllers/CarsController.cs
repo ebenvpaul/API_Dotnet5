dad6f46a24a5db66cd835c9592890b498b9e18ee
Eben Varghese Paul <eben92paul@gmail.com>
2023-10-22 10:28:19 +0530

    dev branch added

---
+++ Controllers/CarsController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Dotnet5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CarsController : ControllerBase
+    {
+         private static readonly string[] Cars = new[]
+    {
+        "Maruti", "BMW", "Audi", "Benz"
+    };
+
+ [HttpGet(Name = "GetCars")]
+    public object Get()
+    {
+        return Cars.ToArray();
+    }
+
+    }
+}
